e83f8a85d56418c93a654101ce5ef0cdad42c8e0
Connor Keith <keithcs@mail.uc.edu>
2019-09-16 20:50:24 -0400

    Turns out I do know what I am doing

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const playButton = document.getElementById("playButton");
-const welcomeScreen = dcoument.getElementById("welcomeScreen");
 const gameScreen = document.getElementById("gameScreen");
+const welcomeScreen = document.getElementById("welcomeScreen");
+const submitButton = document.getElementById("submitButton");
+
+const ROCK_SELECTION = 1;
+const PAPER_SELECTION = 2;
+const SCISSORS_SELECTION = 3;
+
 
 playButton.addEventListener("click", function(){
 
-   
-
+    welcomeScreen.className = "hidden";
+    gameScreen.className = "";
+    
 });
+
+submitButton.addEventListener("submit", function() {
+
+    class RockPaperScissors {
+        constructor() {
+            this.ComputerMove = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+            
+        }
+
+    }
+
+})
+
